f0495caf190acdc5d1c8b11a6d3151df954b5c93
niceoneallround <niceoneallround@gmail.com>
2015-11-23 11:39:22 -0800

    Add test when reading from file

--- test/testCryptoUtils.js
+++ test/testCryptoUtils.js
@@ -7,6 +7,9 @@
 var assert = require('assert'),
     awsKMSUtils = require('../lib/awsKMSUtils'),
     cryptoUtils = require('../lib/cryptoUtils'),
+    fs = require('fs'),         // required to read certs and keys
+    should = require('should'),
+    path = require('path'),
     util = require('util');
 
 describe('Crypto Utils Tests', function() {
@@ -23,15 +26,16 @@ describe('Crypto Utils Tests', function() {
 
   describe('1 encrypt/hmac/encode - decode/checkHmac/decrypt tests', function() {
 
+    var kmsKeyParams = {
+      KeyId: 'arn:aws:kms:us-east-1:835222312890:alias/test_out_kms',
+      KeySpec: 'AES_256',
+      EncryptionContext: {type: 'servicename:resourcename', id: 'none'}
+    };
+
     it('1.1 Test with TEXT', function(done) {
       var params = {};
-
       params.kms = kms;
-      params.kmsKeyParams = {
-        KeyId: 'arn:aws:kms:us-east-1:835222312890:alias/test_out_kms',
-        KeySpec: 'AES_256',
-        EncryptionContext: {type: 'servicename:resourcename', id: 'none'}
-      };
+      params.kmsKeyParams = kmsKeyParams;
       params.plain = 'bob';
 
       cryptoUtils.encryptHMACEncode(params, function(err, sfmt) {
@@ -50,7 +54,35 @@ describe('Crypto Utils Tests', function() {
           done();
         });
       });
-    }); // it 2.1
-  }); // describe 2
+    }); // it 1.1
+
+    it('1.2 Test with Binary', function(done) {
+      var params = {}, dataFile, dataBuffer, startJson;
+      params.kms = kms;
+      params.kmsKeyParams = kmsKeyParams;
+
+      dataFile = path.join(__dirname, './testData.json');
+      dataBuffer = fs.readFileSync(dataFile); // returns raw buffer of file in binary encoding of file
+      params.plain = dataBuffer;
+      startJson = JSON.parse(dataBuffer);
+
+      cryptoUtils.encryptHMACEncode(params, function(err, sfmt) {
+        var decryptParams = {};
+        assert(!err, util.format('unexpected error:%j', err));
+        assert(sfmt, 'no storage format returned');
+
+        decryptParams.kms = kms;
+        decryptParams.sfmt = sfmt;
+        cryptoUtils.decodeHMACDecrypt(decryptParams, function(err, plain) {
+          var endJson;
+          assert(!err, util.format('unexpected error:%j', err));
+          assert(plain, 'no plain returned');
+          endJson = JSON.parse(plain);
+          endJson.should.have.property('prop1', startJson.prop1);
+          done();
+        });
+      });
+    }); // it 1.1
+  }); // describe 1
 
 });
